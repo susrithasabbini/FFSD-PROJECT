47154269c904b1dc2999399adf144484a96f61c4
chandubomma <bommachandugoud@gmail.com>
2023-03-18 16:24:46 +0530

    added menu page

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const foodItems=[
 //<------------------------------temparory static data for food items----------------------------------------------------->
 //<------------------------------temporary static data for restuarants----------------------------------------------------->
 const restuarants=[
-  {image:"/Login/images/login_image_2.png",name:"Hotel Hungrezy",type:"Biryani, Chinese ,Fast Food",rating:"5",time:"15 MIN",cost:"Rs100 for one"},
+  {image:"/Login/images/login_image_2.png",name:"Hotel Hungrezy",type:"Biryani, Chinese ,Fast Food",rating:"5",time:"15 MIN",cost:"Rs100 for one",location:"Sricity"},
   {image:"/Login/images/login_image_2.png",name:"Hotel Hungrezy",type:"Biryani, Chinese ,Fast Food",rating:"5",time:"15 MIN",cost:"Rs100 for one"},
   {image:"/Login/images/login_image_2.png",name:"Hotel Hungrezy",type:"Biryani, Chinese ,Fast Food",rating:"5",time:"15 MIN",cost:"Rs100 for one"},
   {image:"/Login/images/login_image_2.png",name:"Hotel Hungrezy",type:"Biryani, Chinese ,Fast Food",rating:"5",time:"15 MIN",cost:"Rs100 for one"},
@@ -43,8 +43,52 @@ const restuarants=[
   {image:"/Login/images/login_image_2.png",name:"Hotel Hungrezy",type:"Biryani, Chinese ,Fast Food",rating:"5",time:"15 MIN",cost:"Rs100 for one"},
 ]
 
+const recomended = [
+    {image:"/CUSTOMER/Order_Food/images/biryani.jpeg",name:"Biryani",cost:"100",description:"serves 1",type:"nonveg"},
+    {image:"/CUSTOMER/Order_Food/images/pizza.jpeg",name:"Pizza",cost:"50",description:"serves 1",type:"veg"},
+    {image:"/CUSTOMER/Order_Food/images/chicken.jpeg",name:"Chicken",cost:"100",description:"serves 2",type:"nonveg"},
+    {image:"/CUSTOMER/Order_Food/images/biryani.jpeg",name:"Thali",cost:"80",description:"serves 1",type:"veg"},
+    {image:"/CUSTOMER/Order_Food/images/pizza.jpeg",name:"Burger",cost:"40",description:"serves 1",type:"veg"},
+    {image:"/CUSTOMER/Order_Food/images/chicken.jpeg",name:"Ice Cream",cost:"20",description:"serves 1",type:"veg"}
+]
+
+const Menu = [
+    {
+        categoryName : "Chicken",
+        items : [
+            {image:"/CUSTOMER/Order_Food/images/chicken.jpeg",name:"Chicken",cost:"100",description:"serves 2",type:"nonveg"},
+            {image:"/CUSTOMER/Order_Food/images/biryani.jpeg",name:"Biryani",cost:"100",description:"serves 1",type:"nonveg"},
+            {image:"/CUSTOMER/Order_Food/images/chicken.jpeg",name:"Fried Rice",cost:"60",description:"serves 1",type:"nonveg"},
+        ]
+    },
+
+    {
+        categoryName : "Bakery",
+        items : [
+            {image:"/CUSTOMER/Order_Food/images/pizza.jpeg",name:"Cakes",cost:"120",description:"serves 4",type:"nonveg"},
+            {image:"/CUSTOMER/Order_Food/images/chicken.jpeg",name:"Ice Cream",cost:"20",description:"serves 1",type:"veg"},
+            {image:"/CUSTOMER/Order_Food/images/biryani.jpeg",name:"Rolls",cost:"10",description:"serves 1",type:"veg"},
+        ]
+    },
+
+    {
+        categoryName : "Pizza",
+        items : [
+            {image:"/CUSTOMER/Order_Food/images/pizza.jpeg",name:"Pizza",cost:"50",description:"serves 1",type:"veg"},
+            {image:"/CUSTOMER/Order_Food/images/pizza.jpeg",name:"Burger",cost:"40",description:"serves 1",type:"veg"},
+        ]
+    }
+]
+
 // *** GET Routes - display pages ***
 // Root Route
+
+app.get('/', function (req, res) {
+    const pageTitle = "Hungrezy";
+    res.render('pages/index',{pageTitle : pageTitle});
+
+});
+
 app.get('/Restuarants', function (req, res) {
     const pageTitle = "Restuarants";
     res.render('pages/Explore_Restuarants',{foodItems : foodItems,restuarants : restuarants,pageTitle : pageTitle});
@@ -52,9 +96,9 @@ app.get('/Restuarants', function (req, res) {
 });
 
 
-app.get('/', function (req, res) {
-    const pageTitle = "Hungrezy";
-    res.render('pages/index',{pageTitle : pageTitle});
+app.get('/Menu', function (req, res) {
+    const pageTitle = "Menu";
+    res.render('pages/Restuarant_Menu',{Restuarant: restuarants[0],recomended : recomended,Menu : Menu});
 
 });
 
